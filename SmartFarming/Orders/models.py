8e1d8b23053092f2ac43e24f75137e732d0d55f9
catediana <catherinenanyala@gmail.com>
2025-05-27 17:40:21 +0300

    added two models Order and Product models and also created a superuser

--- SmartFarming/Orders/models.py
+++ SmartFarming/Orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from Products.models import Product
 
-# Create your models here.
+#Order model which shows order placed by the customer and manages delivery tracking
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.product.name}"
+
--- SmartFarming/Products/models.py
+++ SmartFarming/Products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+#product model to display the product available
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('Eggs', 'Eggs'),
+        ('Chicks', 'Chicks'),
+        ('Chicken', 'Chicken'),
+    ]
+
+    SUBCATEGORY_CHOICES = [
+        ('Broilers', 'Broilers'),
+        ('Layers', 'Layers'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    subcategory = models.CharField(max_length=50, choices=SUBCATEGORY_CHOICES, blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/', blank=True)
+    description = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
